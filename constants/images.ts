7f5539683338be8bde2b9ccf39a1e99d22d1037d
Slowikxx <aniakslowik@student.agh.edu.pl>
2024-08-27 19:39:58 +0200

    - imported assets (images and fonts) - added custom tailwindcss color and font settings - set up folders structure

---
+++ constants/images.ts
@@ -0,0 +1,88 @@
+// light mode inactive
+import darkBookshelf from '../assets/images/dark_bookshelf.png';
+import darkHome from '../assets/images/dark_home.png';
+import darkProfile from '../assets/images/dark_profile.png';
+import darkShop from '../assets/images/dark_shop.png';
+import darkPlus from '../assets/images/dark_plus.png';
+
+// light mode active
+import darkActiveBookshelf from '../assets/images/dark_active_bookshelf.png';
+import darkActiveHome from '../assets/images/dark_active_home.png';
+import darkActiveProfile from '../assets/images/dark_active_profile.png';
+import darkActiveShop from '../assets/images/dark_active_shop.png';
+
+// light mode shop
+import darkLetters from '../assets/images/dark_letters.png';
+import darkMoney from '../assets/images/dark_money.png';
+import darkShelf from '../assets/images/dark_shelf.png';
+import darkSteal from '../assets/images/dark_steal.png';
+import darkStop from '../assets/images/dark_stop.png';
+import darkSurprise from '../assets/images/dark_surprise.png';
+
+// dark mode inactive
+import lightBookshelf from '../assets/images/light_bookshelf.png';
+import lightHome from '../assets/images/light_home.png';
+import lightProfile from '../assets/images/light_profile.png';
+import lightShop from '../assets/images/light_shop.png';
+import lightPlus from '../assets/images/light_plus.png';
+
+//dark mode active
+import lightActiveBookshelf from '../assets/images/light_active_bookshelf.png';
+import lightActiveHome from '../assets/images/light_active_home.png';
+import lightActiveProfile from '../assets/images/light_active_profile.png';
+import lightActiveShop from '../assets/images/light_active_shop.png';
+
+// dark mode shop
+import lightLetters from '../assets/images/light_letters.png';
+import lightMoney from '../assets/images/light_money.png';
+import lightShelf from '../assets/images/light_shelf.png';
+import lightSteal from '../assets/images/light_steal.png';
+import lightStop from '../assets/images/light_stop.png';
+import lightSurprise from '../assets/images/light_surprise.png';
+
+// baclground
+import lightBackground from '../assets/images/white-background.png';
+import darkBackground from '../assets/images/dark-background.png';
+import lightTornPaper from '../assets/images/torn_paper.png';
+import lightLinedPaper from '../assets/images/lined_paper.png';
+import darkTornPaper from '../assets/images/dark_torn_paper.png';
+import darkLinedPaper from '../assets/images/dark_lined_paper.png';
+
+export default {
+	darkBookshelf,
+	darkHome,
+	darkProfile,
+	darkShop,
+	darkPlus,
+	darkActiveBookshelf,
+	darkActiveHome,
+	darkActiveProfile,
+	darkActiveShop,
+	darkLetters,
+	darkMoney,
+	darkShelf,
+	darkSteal,
+	darkStop,
+	darkSurprise,
+	lightBookshelf,
+	lightHome,
+	lightProfile,
+	lightShop,
+	lightPlus,
+	lightActiveBookshelf,
+	lightActiveHome,
+	lightActiveProfile,
+	lightActiveShop,
+	lightLetters,
+	lightMoney,
+	lightShelf,
+	lightSteal,
+	lightStop,
+	lightSurprise,
+	lightBackground,
+	darkBackground,
+	lightTornPaper,
+	lightLinedPaper,
+	darkTornPaper,
+	darkLinedPaper,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./App.{js,jsx,ts,tsx}',
-		'./<custom directory>/**/*.{js,jsx,ts,tsx}',
-	],
+	content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				gray: {
+					DEFAULT: '#808080',
+					100: '#d7d7d7',
+					200: '#a7a7a7',
+					300: '#626262',
+					400: '#959595',
+				},
+				gold: '#CCB07C',
+				logoutRed: '#EB4040',
+			},
+			fontFamily: {
+				coveredBYG: ['Covered By Your Grace', 'cursive'],
+			},
+		},
 	},
 	plugins: [],
 };
